38b12e1c085a96f5ebdcfd0d28811a5fa8b167c7
I076835 <witalij.rudnicki@sap.com>
2021-02-22 21:31:24 +0000

    modified:   env_tpcds.sh

--- env_tpcds.sh
+++ env_tpcds.sh
@@ -14,4 +14,5 @@ aws_ro_s3user=S3TPC-reader
 aws_key_file_folder=${myTemp_folder}
 aws_key_file=S3TPC-reader-access-key.json
 
+HDB_USE_IDENT=AWSCloudShell
 saphc_hdb_endpoint=8e1a286a-21d7-404d-8d7a-8c77d2a77050.hana.trial-eu10.hanacloud.ondemand.com:443
