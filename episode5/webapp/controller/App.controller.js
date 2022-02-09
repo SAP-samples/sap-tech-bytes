e52907e90494c88c755806dc9b377e8ea91db6d2
nicogeburek <nicolai.geburek@sap.com>
2022-02-09 15:55:13 +0100

    new episode 5

---
+++ episode5/webapp/controller/App.controller.js
@@ -0,0 +1,20 @@
+sap.ui.define([
+   "sap/ui/core/mvc/Controller",
+   "sap/ui/model/json/JSONModel"
+], function (Controller, JSONModel) {
+   "use strict";
+   return Controller.extend("sap.ui.demo.controller.App", {
+      incrementBy1: function () {
+         let myTextElem = this.getView().byId("counter");
+         let myNum = parseInt(myTextElem.getText());
+         let myNewNum = myNum + 1;
+         myTextElem.setText(myNewNum);
+      },
+      onInit: function () {
+         let oModel = new JSONModel(
+            sap.ui.require.toUrl("sap/ui/demo/Data.json")
+         );
+         this.getView().setModel(oModel);
+      }
+   });
+});
