038c142b0445195d2b4c6a92a18c883b15100fae
nicogeburek <nicolai.geburek@sap.com>
2022-02-18 17:54:40 +0100

    added '/another-endpoint'

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 const path = require("path");
 
 const app = express();
+app.use(express.json());
 
 if (process.env.NODE_ENV === "development") {
     console.log("Running in development...");
@@ -11,6 +12,12 @@ if (process.env.NODE_ENV === "development") {
 } else {
     app.use("/", express.static(path.join(__dirname, "./uimodule/dist")));
 }
+
+app.get("/another-endpoint", function(req, res) {
+    res.send({
+        message: "Hi there!"
+    });
+});
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`Server started at http://localhost:${port}`));
