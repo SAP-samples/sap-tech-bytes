f2f61640c3bcad97ee355a4f8f393bec10e19872
nicogeburek <nicolai.geburek@sap.com>
2022-06-15 14:24:57 +0200

    add optional popover

--- episode3/webapp/controller/App.controller.js
+++ episode3/webapp/controller/App.controller.js
@@ -13,6 +13,36 @@ sap.ui.define([
       onInit: function () {
          let oModel = new JSONModel(sap.ui.require.toUrl("sap/ui/demo/Data.json"))
          this.getView().setModel(oModel)
+      },
+      
+      //everything below is for the optional pop-up
+      onAfterRendering: function () {
+         let that = this
+         window.setTimeout(function () {
+            let items = that.byId("episodeOverview").getItems()
+            for (let i = 0; i < items.length; i++) {
+               items[i].addEventDelegate({
+                  onmouseover: that._showPopover,
+                }, that)
+            }
+         }, 1000)
+      },
+      _showPopover: function (oEvent) {
+         let targetNode = oEvent.originalEvent.target
+         let listNode
+         if (targetNode.tagName != "LI") {
+            listNode = targetNode.parentElement.closest('li')
+         } else {
+            listNode = targetNode
+         }
+         let index = listNode.id.split("").reverse().join("").split("-")[0]
+         let oItem = this.byId("episodeOverview").getItems()[index]
+         let oDate = this.getView().getModel().getProperty(oItem.getBindingContext().sPath).published
+         this.byId("popoverText").setText(`Published: ${oDate}`)
+         this.byId("popover").openBy(listNode)
+      },
+      _clearPopover: function () {
+         this.byId("popover").close()
       }
    })
 })
